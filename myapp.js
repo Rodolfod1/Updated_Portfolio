436048976b2b84e144dcde7ee1b1546f4e7170a7
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-11-03 16:47:10 -0500

    defining Jscript and polishing appereance

--- myapp.js
+++ myapp.js
@@ -1,12 +1,49 @@
 var shown=false;
+var hm=false;
+var work,bf,cnt=false;
 
-$(".btn").on("click", function(){
-    if (shown){$("#sideNav").addClass("hide");
-    shown=false;
-    }
-    else{
-        $("#sideNav").removeClass("hide");
-        shown=true;
-    }
-    
-})
+$(document).ready(function(){
+    $(".btn").on("click", function(){
+        if (shown){$("#sideNav").addClass("hide");
+        shown=false;
+        }
+         else {
+             $("#sideNav").removeClass("hide");
+            shown=true;
+         }
+    });
+// if the portfolio is selected
+ $("#portfolio").on("click",function(){
+ if (!work){$("#about").addClass("hide");
+ $("#contacto").addClass("hide");
+ $("#folio").removeClass("hide");
+var work=true;
+ var bf,hm,cnt=false;
+ }
+});
+// if contact is selected then hide portfolio and contact info
+ $("#contact").on("click",function(){
+     if (!cnt){$("#about").addClass("hide");
+     $("#folio").addClass("hide");
+     $("#contacto").removeClass("hide");
+  var   cnt=true;
+  var  bf,hm,work=false;
+     }
+     });
+    //  if the about is selected then hide everything else 
+    $("#casa").on("click",function(){
+        console.log(hm);
+        if (!hm){$("#about").removeClass("hide");
+        $("#folio").addClass("hide");
+        $("#contacto").addClass("hide");
+        var work=true;
+        var bf,hm,cnt=false;
+        }
+        });
+
+
+
+
+
+}); // this is the closing of the main function 
+
